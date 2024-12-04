847a1d8413f64850a2e879cf0d0d88d2f6fcc40b
MatthewFlores2006 <cuentaalternativamatthewflores@gmail.com>
2024-12-03 23:15:41 -0500

    Actualización del ejercicio

--- ProyectoAAB1/src/MonitorFrecuencia.java
+++ ProyectoAAB1/src/MonitorFrecuencia.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class MonitorFrecuencia {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double dia, mes, año, eD, eM, eA, fMax, fEMin, fEMax;
+        double dia, mes, año, eA, fMax, fEMin, fEMax;
         int i = 1, lim, card;
         String nom, niv, ind;
         System.out.print("Ingrese su primer nombre: ");
@@ -13,14 +13,12 @@ public class MonitorFrecuencia {
         mes = in.nextDouble();
         System.out.print("Ingrese su año de nacimiento: ");
         año = in.nextDouble();
-        eD = 30 - dia;
-        eM = 12 - mes;
         eA = 2024 - año;
         fMax = 220 - eA;
         fEMin = fMax * 0.5;
         fEMax = fMax * 0.85;
         System.out.printf("|\t%s\t|\t%s\t|\t%s\t|\t%s\t|", "NOMBRE", "FECHA DE NACIMIENTO", "FRECUENCIA MAXIMA", "FRECUENCIA ESPERADA");
-        System.out.printf("\n|\t%s\t|\t%.2f %s %.2f %s %.2f\t|\t%.2f\t\t\t|\t%.2f %s %.2f\t|\n", nom, eD, "/", eM, "/", eA, fMax, fEMin, " - ", fEMax);
+        System.out.printf("\n|\t%s\t|\t%.2f %s %.2f %s %.2f\t|\t%.2f\t\t\t|\t%.2f %s %.2f\t|\n", nom, dia, "/", mes, "/", año, fMax, fEMin, " - ", fEMax);
         System.out.print("Ingrese el limite de veces que se registra su estado: ");
         lim = in.nextInt();
         while (i <= lim)
@@ -40,3 +38,26 @@ public class MonitorFrecuencia {
         }
     }
 }
+/***
+ * run:
+Ingrese su primer nombre: Matthew
+Ingrese su dia de nacimiento: 2
+Ingrese su mes de nacimiento: 4
+Ingrese su a�o de nacimiento: 2006
+|	NOMBRE	|	FECHA DE NACIMIENTO	|	FRECUENCIA MAXIMA	|	FRECUENCIA ESPERADA	|
+|	Matthew	|	2,00 / 4,00 / 2006,00	|	202,00			|	101,00  -  171,70	|
+Ingrese el limite de veces que se registra su estado: 3
+Ingrese su nivel cardiaco actual: 100
+Agregue un indicador adicional que usted considere relevante: a
+|	TIEMPO	|	RITMO	|	ESTADO	|	INDICADOR	|
+|	1	|	100	|	Ritmo cardiaco demasiado reducido	|	a	|
+Ingrese su nivel cardiaco actual: 130
+Agregue un indicador adicional que usted considere relevante: b
+|	TIEMPO	|	RITMO	|	ESTADO	|	INDICADOR	|
+|	2	|	130	|	Ritmo esperado	|	b	|
+Ingrese su nivel cardiaco actual: 180
+Agregue un indicador adicional que usted considere relevante: c
+|	TIEMPO	|	RITMO	|	ESTADO	|	INDICADOR	|
+|	3	|	180	|	Ritmo cardiaco demasiado elevado	|	c	|
+BUILD SUCCESSFUL (total time: 33 seconds)
+ */
